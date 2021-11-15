a6bc9d4d30fb5563bb8b8b65f8d9c45512c4c842
Anthony Chao <qoosuperman@gmail.com>
2021-11-15 22:35:30 +0800

    feat: update send line notify script with random emoticon

--- send_line_notify/code.js
+++ send_line_notify/code.js
@@ -1,13 +1,27 @@
+var userProperty = PropertiesService.getUserProperties();
+
 function sendLineNotify(messageText) {
-  var userProperty = PropertiesService.getUserProperties();
   // userProperty.setProperty('lineNotifyToken','<your line notify token>')
   var token = userProperty.getProperty('lineNotifyToken')
   var options = {
     "method" : "post",
     "headers" : { "Authorization" : "Bearer " + token },
     "payload" : {
-      "message" : messageText
+      "message" : `${pickEmoticon()}\n${messageText}`
     }
   }
   UrlFetchApp.fetch("https://notify-api.line.me/api/notify", options)
+}
+
+function pickEmoticon() {
+  // userProperty.setProperty('emoticon_sheet','<your line notify token>')
+  let emoticonSheetId = userProperty.getProperty('emoticon_sheet')
+
+  Array.prototype.sample = function() {
+    return this[Math.floor(Math.random()*this.length)];
+  }
+  let sheet = SpreadsheetApp.openById(emoticonSheetId).getActiveSheet()
+  let lastRow = sheet.getLastRow()
+  let emoticons = sheet.getRange(1, 1, lastRow).getValues().flat()
+  return emoticons.sample()
 }
