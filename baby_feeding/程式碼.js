f26cf096a0a0835f306b2c1496858c9b2e32afec
Anthony Chao <qoosuperman@gmail.com>
2025-11-09 16:44:06 +0800

    feat: update feeding script, add all milk column

--- baby_feeding/程式碼.js
+++ baby_feeding/程式碼.js
@@ -4,6 +4,22 @@ let sheetId= userProperty.getProperty('poo_pee_sheet_id')
 let spreadSheet = SpreadsheetApp.openById(sheetId)
 
 function onFormSubmit(e) {
+  // 增加錯誤處理和除錯資訊
+  Logger.log('Event object: ' + JSON.stringify(e));
+
+  // 檢查事件物件是否存在
+  if (!e) {
+    Logger.log('Error: Event object is undefined');
+    return;
+  }
+
+  // 檢查 response 是否存在
+  if (!e.response) {
+    Logger.log('Error: e.response is undefined');
+    Logger.log('Available properties: ' + Object.keys(e));
+    return;
+  }
+
   var response = e.response;
   var formResponses = response.getItemResponses();
   // // 名字
@@ -12,6 +28,8 @@ function onFormSubmit(e) {
 
   // 日期，若為空字串 fallback 回今天日期
   var dateText = formResponses[1].getResponse();
+  Logger.log(dateText)
+  Logger.log(typeof(dateText))
   if(dateText === '') {
     date = new Date()
   } else {
@@ -21,6 +39,8 @@ function onFormSubmit(e) {
 
   // 時間
   var timeText = formResponses[2].getResponse();
+    Logger.log(timeText)
+  Logger.log(typeof(timeText))
 
   let dateTimeObject = new Date(recordDateText + ' ' + timeText)
   recordDateTimeText = Utilities.formatDate(new Date(dateTimeObject), 'GMT+8', 'yyyy/MM/dd HH:mm')
@@ -28,10 +48,16 @@ function onFormSubmit(e) {
   // 配方奶幾 ml
   var infantFormula = formResponses[3].getResponse();
   infantFormula = parseInt(infantFormula)
+  Logger.log(infantFormula)
+  Logger.log(typeof(infantFormula))
 
   // 母奶幾 ml
   var breastMilk = formResponses[4].getResponse();
   breastMilk = parseInt(breastMilk)
+  Logger.log(breastMilk)
+  Logger.log(typeof(breastMilk))
 
-  mainSheet.appendRow([recordDateTimeText, infantFormula, breastMilk])
+  var allMilk = infantFormula + breastMilk
+
+  mainSheet.appendRow([recordDateTimeText, infantFormula, breastMilk, allMilk])
 }
